c2bd87750048924698e002d7a65c643a24bc077d
flamelee2000 <flamelee2000@gmail.com>
2016-01-30 17:47:45 +0800

    merge two projects
    merge login project and picshow project

--- FlameJavaFXLoginPicShow/src/flameJavaFXLoginPicShow/PicShow.java
+++ FlameJavaFXLoginPicShow/src/flameJavaFXLoginPicShow/PicShow.java
@@ -1,10 +1,4 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
  * Copyright (c) 2008, 2012 Oracle and/or its affiliates.
  * All rights reserved. Use is subject to license terms.
  *
@@ -35,23 +29,8 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package flameJavaFXLoginPicShow;
 
-import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.fxml.JavaFXBuilderFactory;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
-import flameJavaFXLoginPicShow.model.User;
-import flameJavaFXLoginPicShow.security.Authenticator;
 import javafx.animation.Interpolator;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
@@ -80,88 +59,6 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 /**
- *
- * @author Flame
- */
-public class FlameJavaFXLoginPicShow extends Application {
-
-    private Stage stage;
-    private User loggedUser;
-    private final double MINIMUM_WINDOW_WIDTH = 100.0;
-    private final double MINIMUM_WINDOW_HEIGHT = 300.0;
-
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            stage = primaryStage;
-            stage.setTitle("Login Window");
-            stage.setMinWidth(MINIMUM_WINDOW_WIDTH);
-            stage.setMinHeight(MINIMUM_WINDOW_HEIGHT);
-            gotoLogin();
-            stage.show();
-        } catch (Exception ex) {
-            Logger.getLogger(FlameJavaFXLoginPicShow.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Application.launch(FlameJavaFXLoginPicShow.class, (java.lang.String[]) null);
-    }
-
-    private void gotoLogin() {
-        try {
-            LoginController login = (LoginController) replaceSceneContent("login.fxml");
-            login.setApp(this);
-        } catch (Exception ex) {
-            Logger.getLogger(FlameJavaFXLoginPicShow.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    public boolean userLogging(String userId, String password) {
-        if (Authenticator.validate(userId, password)) {
-            loggedUser = User.of(userId);
-            gotoMainWindow();
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private Initializable replaceSceneContent(String fxml) throws Exception {
-        FXMLLoader loader = new FXMLLoader();
-        InputStream in = FlameJavaFXLoginPicShow.class.getResourceAsStream(fxml);
-        loader.setBuilderFactory(new JavaFXBuilderFactory());
-        loader.setLocation(FlameJavaFXLoginPicShow.class.getResource(fxml));
-        AnchorPane page;
-        try {
-            page = (AnchorPane) loader.load(in);
-        } finally {
-            in.close();
-        }
-        Scene scene = new Scene(page, 560, 420);
-        stage.setScene(scene);
-        stage.sizeToScene();
-        return (Initializable) loader.getController();
-    }
-
-    private void gotoMainWindow() {
-        try {
-            Circle circ = new Circle(40, 40, 30);
-            Group root = new Group(circ);
-            Scene scene = new Scene(root, 800, 600);
-            stage.setTitle("Main Window");
-//            stage.setScene(scene);
-        initPicShow(stage);
-        stage.show();
-        } catch (Exception ex) {
-            Logger.getLogger(FlameJavaFXLoginPicShow.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
- /**
  * A display shelf of images using the PerspectiveTransform effect.
  *
  * @see javafx.scene.effect.PerspectiveTransform
@@ -184,6 +81,7 @@ public class FlameJavaFXLoginPicShow extends Application {
  * @resource animal13.jpg
  * @resource animal14.jpg
  */
+public class PicShow extends Application {
     private static final double WIDTHPICSHOW = 495, HEIGHTPICSHOW = 300;
 
     private void initPicShow(Stage primaryStage) {
@@ -410,5 +308,20 @@ public class FlameJavaFXLoginPicShow extends Application {
             getChildren().addAll(imageView);
         }
     }
-    
+
+    @Override public void start(Stage stagePicShow) throws Exception {
+        initPicShow(stagePicShow);
+        stagePicShow.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX 
+     * application. main() serves only as fallback in case the 
+     * application can not be launched through deployment artifacts,
+     * e.g., in IDEs with limited FX support. NetBeans ignores main().
+     * @param args the command line arguments
+     */
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
 }
